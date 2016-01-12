24af745c048b97a119efef6badc701d44ac175bc
Guillaume <glipsl@ipsl.jussieu.fr>
2016-01-12 11:58:29 +0100

    Initial commit

---
+++ esgissuemanager/manager.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+   :platform: Unix
+   :synopsis: Manages ESGF issues on GitHub repository.
+
+"""
+
+from github import Github
+
+
+class ESGFIssue(object):
+
+    def __init__(self):
+
+    def _create(self, args):
+
+    def _update(self, args):
+
+    def _close(self, args):
+
+    def _remove(self, args):
